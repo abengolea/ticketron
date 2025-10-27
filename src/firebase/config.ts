faa84b135e23e99e14deccd6fa4576583a26ca78
Adrian Bengolea <abengolea1@gmail.com>
2025-10-27 02:19:07 +0000

    no necesitas esta info : Sobrenombre de la app Ticketron app ID de la ap

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-9893505602-68edc",
-  "appId": "1:978726699068:web:54e51160b95ab11d594339",
+  "appId": "1:978726699068:web:aa858650256f3628594339",
   "apiKey": "AIzaSyDm1C-IgdbKPKOA831B1zkF0Z00TOmRgn4",
   "authDomain": "studio-9893505602-68edc.firebaseapp.com",
-  "measurementId": "",
+  "storageBucket": "studio-9893505602-68edc.appspot.com",
   "messagingSenderId": "978726699068"
 };
